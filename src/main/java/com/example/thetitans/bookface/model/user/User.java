bf51946bc3272cb82f00fe2428be7369e219c03a
tranminhthong <minhthong9711@gmail.com>
2021-05-19 16:20:26 +0700

    update

--- src/main/java/com/example/thetitans/bookface/model/user/User.java
+++ src/main/java/com/example/thetitans/bookface/model/user/User.java
@@ -4,10 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
@@ -38,7 +35,7 @@ public class User {
     private String hobbies;
 
     private String about;
-
+    @Lob
     private String avatar;
 
     private String phone;
