92d3576b4d46aca12912f000c75f1aab3048d018
Luthanh2210 <72242686+Luthanh2210@users.noreply.github.com>
2021-05-20 13:38:15 +0700

    post v1

--- src/main/java/com/example/thetitans/bookface/controller/post/PostController.java
+++ src/main/java/com/example/thetitans/bookface/controller/post/PostController.java
@@ -1,11 +1,21 @@
 package com.example.thetitans.bookface.controller.post;
+import com.example.thetitans.bookface.model.post.Comment;
 import com.example.thetitans.bookface.model.post.Post;
 import com.example.thetitans.bookface.service.user.impl.PostService;
+import javafx.geometry.Pos;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.springframework.web.servlet.function.ServerResponse.status;
 
 @RestController
 @RequestMapping("/posts")
@@ -16,11 +26,37 @@ public class PostController {
     @Autowired
     private final PostService postService;
 
-    //region api status post
-    @PostMapping
-    @ResponseBody
-    public ResponseEntity<Post> createStatus(@RequestBody Post post) {
-        Post status = postService.save(post);
-        return new ResponseEntity(status, HttpStatus.CREATED);
+    @GetMapping
+    public ResponseEntity<Iterable<Post>> getAllPost() {
+        Iterable<Post> posts = postService.findAll();
+        return new ResponseEntity<>(posts, HttpStatus.OK);
+    }
+
+    @GetMapping("/find/{id}")
+    public ResponseEntity<Post> findPostById(@PathVariable Long id) {
+        Post post = null;
+        Optional<Post> optionalPost = postService.findById(id);
+        if(optionalPost.isPresent()){
+            post = optionalPost.get();
+        }
+        return new ResponseEntity<>(post, HttpStatus.OK);
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<Post> createPost(@RequestBody Post post) {
+        Post newPost = postService.save(post);
+        return new ResponseEntity<>(newPost, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<Post> updatePost(@RequestBody Post post) {
+        Post updatePost = postService.save(post);
+        return new ResponseEntity<>(updatePost, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<Post> deletePost(@PathVariable Long id) {
+        postService.delete(id);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
