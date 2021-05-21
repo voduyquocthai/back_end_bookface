11af804de6237a69b8e5444b578721bf363ab624
Nguyễn Khắc Qui <hivongmotloihua@gmail.com>
2021-05-21 09:27:28 +0700

    qui pull từ dev

--- src/main/java/com/example/thetitans/bookface/controller/FriendShipController.java
+++ src/main/java/com/example/thetitans/bookface/controller/FriendShipController.java
@@ -16,19 +16,32 @@ public class FriendShipController {
     private final IFriendshipService friendshipService;
 
     @PostMapping("/add-waiting")
-    public ResponseEntity<?> addFiend(@RequestBody Friendship friendship) {
+    public ResponseEntity<Friendship> addFiend(@RequestBody Friendship friendship) {
         friendshipService.save(friendship);
-        return new ResponseEntity<>(HttpStatus.OK);
+        Friendship friendship1 = friendshipService.findFriendshipBySenderUserIdAndReceiverUserId(
+                                    friendship.getSender().getUserId(),
+                                    friendship.getReceiver().getUserId());
+        return new ResponseEntity<Friendship>(friendship1,HttpStatus.OK);
     }
     @PostMapping("/add-accept")
-    public ResponseEntity<?> acceptAddFriend(@RequestBody Friendship friendship) {
+    public ResponseEntity<Friendship> acceptAddFriend(@RequestBody Friendship friendship) {
+        Long id1 = friendship.getSender().getUserId();
+        Long id2 = friendship.getReceiver().getUserId();
+        friendship.setStatus(true);
         friendshipService.save(friendship);
-        return new ResponseEntity<>(HttpStatus.OK);
+        Friendship friendship1 = friendshipService.searchIfExistFriends(id1,id2);
+        return new ResponseEntity<Friendship>(friendship1,HttpStatus.OK);
     }
 
     @GetMapping("/unfriend/{id}")
-    public ResponseEntity<?> unfriend(@PathVariable Long id) {
+    public ResponseEntity<Friendship> unfriend(@PathVariable Long id) {
         friendshipService.delete(id);
         return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @GetMapping("/search/{senderUserId}/{receiverUserId}")
+    public ResponseEntity<Friendship> searchFriend(@PathVariable Long senderUserId,@PathVariable Long receiverUserId) {
+        Friendship friendship = friendshipService.searchIfExistFriends(senderUserId,receiverUserId);
+        return new ResponseEntity<Friendship>(friendship,HttpStatus.OK);
     }
 }
