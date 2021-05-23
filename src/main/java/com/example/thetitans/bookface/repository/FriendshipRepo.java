2170887beb203f4a71d5c5fb5fe4c4655e71457b
tranminhthong <minhthong9711@gmail.com>
2021-05-24 00:33:04 +0700

    thêm limit cho FriendshipRepo

--- src/main/java/com/example/thetitans/bookface/repository/FriendshipRepo.java
+++ src/main/java/com/example/thetitans/bookface/repository/FriendshipRepo.java
@@ -12,6 +12,6 @@ public interface FriendshipRepo extends JpaRepository<Friendship, Long> {
         Friendship findFriendshipBySenderUserIdAndReceiverUserId(Long senderUserId, Long ReceiverUserId);
     @Query(value = "SELECT * FROM friendship where (sender_user_id = ?1 and receiver_user_id = ?2)\n" +
             " or" +
-            "\n (receiver_user_id = ?1 and sender_user_id = ?2);",nativeQuery = true)
+            "\n (receiver_user_id = ?1 and sender_user_id = ?2) limit 1;",nativeQuery = true)
     Friendship searchIfExistFriends(Long senderUserId, Long ReceiverUserId);
 }
