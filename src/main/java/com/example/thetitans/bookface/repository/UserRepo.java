6e1f31b1fd2450bbd7be58c1f5bd514f8b822c0f
voduyquocthai <voduyquocthai@gmail.com>
2021-05-23 21:54:31 +0700

    add profile access

--- src/main/java/com/example/thetitans/bookface/repository/UserRepo.java
+++ src/main/java/com/example/thetitans/bookface/repository/UserRepo.java
@@ -3,9 +3,11 @@ package com.example.thetitans.bookface.repository;
 import com.example.thetitans.bookface.model.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepo extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
 
