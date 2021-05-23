bf40ef472f3c7630fca026663cc490d1e83113fb
tranminhthong <minhthong9711@gmail.com>
2021-05-23 23:48:49 +0700

    change dialect

--- src/main/java/com/example/thetitans/bookface/repository/FriendshipRepo.java
+++ src/main/java/com/example/thetitans/bookface/repository/FriendshipRepo.java
@@ -10,7 +10,7 @@ import org.springframework.transaction.annotation.Transactional;
 public interface FriendshipRepo extends JpaRepository<Friendship, Long> {
 //    @Query(value = "SELECT * FROM bookface.friendship where sender_user_id = ? and receiver_user_id = ?;",nativeQuery = true))
         Friendship findFriendshipBySenderUserIdAndReceiverUserId(Long senderUserId, Long ReceiverUserId);
-    @Query(value = "SELECT * FROM bookface.friendship where (sender_user_id = ?1 and receiver_user_id = ?2)\n" +
+    @Query(value = "SELECT * FROM friendship where (sender_user_id = ?1 and receiver_user_id = ?2)\n" +
             " or" +
             "\n (receiver_user_id = ?1 and sender_user_id = ?2);",nativeQuery = true)
     Friendship searchIfExistFriends(Long senderUserId, Long ReceiverUserId);
