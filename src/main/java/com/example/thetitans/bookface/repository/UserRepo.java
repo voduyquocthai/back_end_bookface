9d2e0da76ea6451818dfd082672a7271f794c4a7
voduyquocthai <voduyquocthai@gmail.com>
2021-05-24 14:53:45 +0700

    fix bug

--- src/main/java/com/example/thetitans/bookface/repository/UserRepo.java
+++ src/main/java/com/example/thetitans/bookface/repository/UserRepo.java
@@ -13,8 +13,8 @@ public interface UserRepo extends JpaRepository<User, Long> {
 
     Optional<User> findByUserId(Long id);
 
-    @Query(value = "select * from mxh_bookface.app_user join mxh_bookface.friendship on mxh_bookface.friendship.receiver_user_id = mxh_bookface.user.user_id where mxh_bookface.friendship.sender_user_id =?1 and mxh_bookface.friendship.status=true\n" +
+    @Query(value = "select * from mxh_bookface.app_user join mxh_bookface.friendship on mxh_bookface.friendship.receiver_user_id = mxh_bookface.app_user.user_id where mxh_bookface.friendship.sender_user_id =?1 and mxh_bookface.friendship.status=true\n" +
             "union all\n" +
-            "select * from mxh_bookface.user join mxh_bookface.friendship on mxh_bookface.friendship.sender_user_id = mxh_bookface.user.user_id where mxh_bookface.friendship.receiver_user_id =?1 and mxh_bookface.friendship.status=true\n",nativeQuery = true)
+            "select * from mxh_bookface.app_user join mxh_bookface.friendship on mxh_bookface.friendship.sender_user_id = mxh_bookface.app_user.user_id where mxh_bookface.friendship.receiver_user_id =?1 and mxh_bookface.friendship.status=true\n",nativeQuery = true)
     Iterable<User> findAllFriend(Long id);
 }
