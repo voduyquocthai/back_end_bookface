7bac70cc3d8e0a91ab755a47ee612276ed0e8faf
Luthanh2210 <72242686+Luthanh2210@users.noreply.github.com>
2021-05-25 09:45:25 +0700

    api emontion done

--- src/main/java/com/example/thetitans/bookface/repository/EmotionRepo.java
+++ src/main/java/com/example/thetitans/bookface/repository/EmotionRepo.java
@@ -1,6 +1,7 @@
 package com.example.thetitans.bookface.repository;
 
 import com.example.thetitans.bookface.model.emotion.Emotion;
+import com.example.thetitans.bookface.model.emotion.EmotionType;
 import com.example.thetitans.bookface.model.post.Post;
 import com.example.thetitans.bookface.model.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -12,5 +13,5 @@ import java.util.Optional;
 @Repository
 public interface EmotionRepo extends JpaRepository<Emotion, Long> {
 
-    Optional<Emotion> findTopByPostAndUserOrderByEmotionIdDesc(Post post, User currentUser);
+    Optional<Emotion> findEmotionByPostAndUserAndEmotionType(Post post, User currentUser,EmotionType emotionType);
 }
